9023f63a16ae75a627be2ec10babd1ed8f2bb0b6
Etienne Parent <etienne.parent@beon.ca>
2020-06-09 13:48:14 -0400

    All scenes are grouped in a dictionary. Will try to add to possible.

--- yaml_testing.py
+++ yaml_testing.py
@@ -22,3 +22,38 @@ def yaml_getter(filename):
     that contain '---' AND one of the possible classes.
     filename: The name of the YAML file to parse.
     returns: The parsed YAML file.
+    '''
+    with open(filename, 'r') as stream:
+        yaml_file = stream.read()
+
+    parsed = []
+    splitted = filter(None, yaml_file.split('--- !'))
+    for element in splitted:
+        if element.strip != '' or element.strip != ' ':
+            parsed.append('--- !' + element)
+    return parsed
+todo = yaml_getter('yaml_testing.yaml')
+print(todo)
+
+def object_creator(parsed_yaml):
+    '''
+    This function creates objects following instructions in the 
+    YAML file. These objects can later be used to create a video.
+    parsed_yaml: A list of strings that allow the creation of 
+    classes.
+    returns: List of objects. Objects possibilities are described 
+    in the 'possible' list.
+    '''  
+    objects = {}
+    for i in range(len(parsed_yaml)):
+        key_name = 'scene%s'%(i)
+        objects[key_name] = yaml.safe_load(parsed_yaml[i])
+
+    return objects
+
+x=object_creator(todo)
+print(x)
+print(x['scene0'])
+
+
+        
